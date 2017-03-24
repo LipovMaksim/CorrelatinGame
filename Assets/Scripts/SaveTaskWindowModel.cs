fe8dc8617f71eb8295f1ef4814559b1d7a6bb6c7
mlipov <rfvbc007@gmail.com>
2017-03-24 23:35:05 +0300

    Added some filese

---
+++ Assets/Scripts/SaveTaskWindowModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveTaskWindowModel : MonoBehaviour {
+
+	[SerializeField]
+	private TaskNote taskNote;
+
+	void Awake () {
+		FileWorker.readLevelFromFileForNote ("E:\\Google Drive\\Диплом\\CorrelatinGame\\Components\\Levels\\test.cglvl", ref taskNote);
+	}
+
+}
---
+++ Assets/Scripts/TaskNote.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskNote : MonoBehaviour {
+	[SerializeField]
+	public GameField icon;
+	[SerializeField]
+	private TextMesh titleMesh;
+	[SerializeField]
+	private TextMesh objectsCountMesh;
+	[SerializeField]
+	private TextMesh descriptionMesh;
+
+	public const float iconW = 2f;
+	public const float iconH = 1.4f;
+	public const string objectCountString = "Количество элементов: ";
+
+	public void setIcon (Texture2D ic) {
+		
+	}
+
+	public void setTitle (string title) {
+		titleMesh.text = title;
+	}
+
+	public void setObjectsCount (int c) {
+		objectsCountMesh.text = objectCountString + c;
+	}
+
+	public void setDescription (string description) {
+		descriptionMesh.text = description;
+	}
+}
